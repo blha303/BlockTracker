6b9efa12cc38138b7c144f6dbeb6f77135b336d2
Steven Smith <stevensmith.ome@gmail.com>
2013-01-15 04:08:11 +0800

    v1.1

--- src/me/blha303/BlockTracker.java
+++ src/me/blha303/BlockTracker.java
@@ -20,23 +20,60 @@ public class BlockTracker extends JavaPlugin implements Listener {
 	// Cheers to einand and hdon ;)
 
 	public void onEnable() {
+//		getConfig().addDefault("automaticUpdates", true);
+//		getConfig().options().copyDefaults(true);
+//		saveConfig();
+//		if (getConfig().getBoolean("automaticUpdates")) {
+//			Updater updater = new Updater(this, "blocktracker", this.getFile(), Updater.UpdateType.DEFAULT, false);
+//			Updater.UpdateResult result = updater.getResult();
+//			switch (result) {
+//			case SUCCESS:
+//				this.getLogger().info("New update for BlockTracker was found!");
+//				this.getLogger().info("Downloaded successfully!");
+//				this.getLogger().info("BlockTracker will be updated next time you restart or reload the server.");
+//				break;
+//			case FAIL_DOWNLOAD:
+//				this.getLogger().info("There is an update for BlockTracker, but I can't download it for some reason.");
+//				this.getLogger().info("Please go to http://dev.bukkit.org/server-mods/blocktracker to get the update.");
+//				break;
+//			case FAIL_DBO:
+//				this.getLogger().info("Unable to check for updates.");
+//				break;
+//			default:
+//				break;
+//			}
+//		} else {
+//			Updater updater = new Updater(this, "blocktracker", this.getFile(), Updater.UpdateType.NO_DOWNLOAD, false);
+//			Updater.UpdateResult result = updater.getResult();
+//			switch(result) {
+//			case UPDATE_AVAILABLE:
+//				this.getLogger().info("There is an update available, but you've disabled automatic updates.");
+//				break;
+//			default:
+//				break;
+//			}
+//		}
 		getServer().getPluginManager().registerEvents(this, this);
 	}
 
 	@EventHandler
 	public void onBlockBreak(BlockBreakEvent event) {
-		if (event.getPlayer().getGameMode() == GameMode.CREATIVE 
-				|| event.getPlayer().getItemInHand().containsEnchantment(Enchantment.SILK_TOUCH)) {
+		if (event.getPlayer().getGameMode() == GameMode.CREATIVE
+				|| event.getPlayer().getItemInHand()
+						.containsEnchantment(Enchantment.SILK_TOUCH)) {
 			return;
 		}
 		event.setCancelled(true);
+		String brokenwith = event.getPlayer().getItemInHand().getType().toString()
+				.toLowerCase().replaceAll("air", "their hand");
 		Location loc = event.getBlock().getLocation();
 		List<String> newlore = new ArrayList<String>();
-		newlore.add("Broken by: " + event.getPlayer().getName());
+		newlore.add("Broken by: " + event.getPlayer().getName() + " with "
+				+ brokenwith);
 		// newlore.add("at " + loc.getBlockX() + ":" + loc.getBlockY() + ":"
 		// + loc.getBlockZ());
-		Collection<ItemStack> drops = 
-				event.getBlock().getDrops(event.getPlayer().getItemInHand());
+		Collection<ItemStack> drops = event.getBlock().getDrops(
+				event.getPlayer().getItemInHand());
 		loc.getBlock().setType(Material.AIR);
 		for (ItemStack i : drops) {
 			ItemMeta im = i.getItemMeta();
