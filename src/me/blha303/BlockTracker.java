0c91b6def5a90d02ad858bcc3c366c530540091a
Steven Smith <stevensmith.ome@gmail.com>
2013-01-14 23:36:06 +0800

    account for silk touch

--- src/me/blha303/BlockTracker.java
+++ src/me/blha303/BlockTracker.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -17,23 +18,25 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class BlockTracker extends JavaPlugin implements Listener {
 
 	// Cheers to einand and hdon ;)
-	
+
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
 	}
 
 	@EventHandler
 	public void onBlockBreak(BlockBreakEvent event) {
-		if (event.getPlayer().getGameMode() == GameMode.CREATIVE) {
+		if (event.getPlayer().getGameMode() == GameMode.CREATIVE 
+				|| event.getPlayer().getItemInHand().containsEnchantment(Enchantment.SILK_TOUCH)) {
 			return;
 		}
 		event.setCancelled(true);
-		Collection<ItemStack> drops = event.getBlock().getDrops(event.getPlayer().getItemInHand());
 		Location loc = event.getBlock().getLocation();
 		List<String> newlore = new ArrayList<String>();
 		newlore.add("Broken by: " + event.getPlayer().getName());
-//		newlore.add("at " + loc.getBlockX() + ":" + loc.getBlockY() + ":"
-//				+ loc.getBlockZ());
+		// newlore.add("at " + loc.getBlockX() + ":" + loc.getBlockY() + ":"
+		// + loc.getBlockZ());
+		Collection<ItemStack> drops = 
+				event.getBlock().getDrops(event.getPlayer().getItemInHand());
 		loc.getBlock().setType(Material.AIR);
 		for (ItemStack i : drops) {
 			ItemMeta im = i.getItemMeta();
@@ -41,5 +44,6 @@ public class BlockTracker extends JavaPlugin implements Listener {
 			i.setItemMeta(im);
 			loc.getWorld().dropItemNaturally(loc, i);
 		}
+		return;
 	}
 }
